ad44f545ded7f1dd611262263dc208ba2708ff05
RizkiMufrizal <mufrizalrizki@gmail.com>
2023-01-21 19:17:56 +0700

    add config http logger

---
+++ common/logger.go
@@ -0,0 +1,34 @@
+package common
+
+import (
+	"github.com/RizkiMufrizal/gin-clean-architecture/exception"
+	"github.com/sirupsen/logrus"
+	"io"
+	"os"
+	"time"
+)
+
+func NewLogger() *logrus.Logger {
+	logger := logrus.New()
+	logger.SetLevel(logrus.InfoLevel)
+	logger.SetFormatter(&logrus.JSONFormatter{
+		FieldMap: logrus.FieldMap{
+			logrus.FieldKeyTime: "@timestamp",
+			logrus.FieldKeyMsg:  "message",
+		},
+	})
+
+	if _, err := os.Stat("logs"); os.IsNotExist(err) {
+		err := os.Mkdir("logs", 0770)
+		exception.PanicLogging(err)
+	}
+
+	date := time.Now()
+	logFile, err := os.OpenFile("logs/log_"+date.Format("01-02-2006_15")+".log", os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0666)
+	exception.PanicLogging(err)
+	if err == nil {
+		multiWriter := io.MultiWriter(os.Stdout, logFile)
+		logger.SetOutput(multiWriter)
+	}
+	return logger
+}
